85206ca5df7d8933ff312308c1d64c55497aa67f
Jairo Ayllon <jairo.josias14@gmail.com>
2025-01-20 15:50:26 -0500

    almacenar nombres en arrays

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// 1. Declarar un array para almacenar los nombres
+let amigos = [];
